5238bb0d5e422fa204e36c7b4b82fa695d2440a6
aziz.zaaine <zaaine@live.fr>
2023-11-20 23:18:05 +0100

    optimisation du code / Ajout commentaires . Changements des appelations variable et class pour meilleurs lecture et comprehension. Rajout class banner-txt.

--- scripts/script.js
+++ scripts/script.js
@@ -1,75 +1,111 @@
-
-// console.log ("fichier activé")
+// création du carroussel sur la bannière :
 
 const slides = [
-  {
-    image: "slide1.jpg",
-    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-  },
-  {
-    image: "slide2.jpg",
-    tagLine:
-      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  },
-  {
-    image: "slide3.jpg",
-    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  },
-  {
-    image: "slide4.png",
-    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  },
-];
-
-let position = 0;
-let listSlides = slides.length;
-
-
-
-// Création des variables pour flèches gauche et droite du carrousel
-
-const flecheDroiteCarrousel = document.querySelector(".arrow_right");
-const flecheGaucheCarrousel = document.querySelector(".arrow_left");
-
-// console log pour savoir quel bouton est click :
-flecheDroiteCarrousel.addEventListener("click", (event) => {
-  console.log("click fleche droite");
-});
-
-flecheGaucheCarrousel.addEventListener("click", (event) => {
-  console.log("click fleche gauche");
-});
-
-// création de la variable Dot
-
-function addDots (){
-	const dot = document.querySelector(".dots")
-	// console.log(dot)
-	for ( let index = 0 ; index < slides.length ; index++) {
-		let dots = document.createElement ("div");
-		dots.setAttribute("class", "dot"); 
-		dot.appendChild(dots)
-		console.log(dots)
+	{
+	  image: "slide1.jpg",
+	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+	},
+	{
+	  image: "slide2.jpg",
+	  tagLine:
+		"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+	},
+	{
+	  image: "slide3.jpg",
+	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+	},
+	{
+	  image: "slide4.png",
+	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+	},
+  ];
+  
+  // copie de valeurs pour utilisation sans altération
+  
+  let position = 0;
+  const listeSlides = slides.length;
+  
+  
+  // Création des flèches navivation interne carrousel
+  
+  const arrowLeft = document.querySelector(".arrow_left");
+  const arrowRight = document.querySelector(".arrow_right");
+  
+  
+  
+  arrowLeft.addEventListener("click",(event) => {
+	  console.log("left");
+	if (position == 0) {
+	  position = listeSlides - 1;
+	} else {
+	  position--;
 	}
-
-}
-
-addDots()
-
-// création d'une fonction qui va ajouté une class list sur la slide en cours
-
-function dotSelected () {
-	const listDots = document.querySelectorAll(".dot"); 
-	for ( let index = 0; index < listDots.length ; index ++) {
-		const dot = listDots[index] ; 
-	if (index == position){
-		dot.classList.add ("dot_selected")
-	}	else {
-		dot.classList.remove("dot_selected")
+	actioncarrousel(position);
+  });
+  
+  
+  arrowRight.addEventListener("click", (event) => {
+	  console.log("Right");
+	if (position == listeSlides - 1) {
+	  position = 0;
+	} else {
+	  position++;
 	}
+  
+	actioncarrousel(position);
+  });
+  
+  
+  
+  // Mise en place des dots pour avoir autant de dots que de slides
+  
+  const dots = document.querySelector(".dots");
+  // console.log('dots');
+  
+  function createDots() {
+	// Je créer la boucle qui va générer autant de dots que de slides
+	for (let index = 0; index < slides.length; index++) {
+	  const dot = document.createElement("div");
+	  dot.setAttribute("class", "dot");
+	  dots.appendChild(dot);
+	  console.log(dot);
 	}
-
-}
-
-dotSelected ()
-
+  }
+  
+  createDots();
+  
+  
+  // Je créer la fonction qui va actualisé le dot avec la class selected
+  
+  
+  
+  function selectedDot() {
+	const dotList = document.querySelectorAll(".dot");
+	for (let index = 0; index < dotList.length; index++) {
+	  const dot = dotList[index];
+	  if (index == position) {
+		dot.classList.add("dot_selected");
+	  } else {
+		dot.classList.remove("dot_selected");
+	  }
+	}
+  }
+  
+  selectedDot();
+  
+  function actioncarrousel(position) {
+	const element = slides[position];
+	// console.log();
+	const img = document.querySelector(".banner-img");
+	img.setAttribute("src", "/assets/images/slideshow/" + element.image);
+	console.log(img);
+  
+	const p = document.querySelector(".banner-txt");
+	p.innerHTML = element.tagLine;
+	// console.log(tagLines);
+  
+	selectedDot();
+  }
+  
+  
+  actioncarrousel(position)
